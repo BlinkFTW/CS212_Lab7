26a1fc389f12b6ce29a76d7c06afa327242ee68a
Eric J. Kang <ekang5@binghamton.edu>
2014-08-28 19:16:37 -0700

    Main

---
+++ lab7.cpp
@@ -0,0 +1,37 @@
+/*
+Eric Kang
+Section 53
+Lab 7
+
+Description:
+	This program allows the user to input a fully parenthesised expression consisting
+	of 0123456789()*+-/ and evalues the expression.
+
+Input(s):
+	eqn[] (char array)
+
+Output(s):
+	intspeek(valStack) (int)
+
+Function(s):
+	void getInput(char *quesPtr);
+	int evalExp( char ques[], int max );
+	int precidence(char x);
+	void printAns( char ques[], int ans );
+
+Dev C++
+codeblocks
+*/
+
+#include<iostream>
+#include<stack>
+#include<queue>
+#include "lab7.h"
+using namespace std;
+
+
+
+int main()
+{
+	Equation lab7;
+}
